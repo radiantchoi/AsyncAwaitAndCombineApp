5539456337349fd3b61d883f094a22afca50f73f
Gordon Choi <thefirate@gmail.com>
2024-01-03 18:36:03 +0900

    Initial Commit

---
+++ AsyncAwaitAndCombineApp/AsyncAwaitAndCombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitAndCombineAppApp.swift
+//  AsyncAwaitAndCombineApp
+//
+//  Created by Gordon Choi on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitAndCombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
